a596bbf57c90d7ecf959de1ee7c0e4d846eaa748
corbamico <corbamico@163.com>
2014-03-17 17:55:27 +0800

    CANTFSHost.Init

--- src/antfs_garmin.cpp
+++ src/antfs_garmin.cpp
@@ -10,11 +10,69 @@ public:
     BOOL Init()
     {
         BOOL bResult;
-        bResult = hostImp_.Init(0,57600);
-        if(!bResult)
+        ANTFS_RETURN eReturn;
+
+        ANTFS_DEVICE_PARAMETERS stSearchMask;
+        ANTFS_DEVICE_PARAMETERS stSearchParameters;
+
+        //Get search device parameters
+        ULONG ulDeviceID = 0;
+        USHORT usManufacturerID = 0;
+        USHORT usDeviceType = 1;
+
+        //Search Mask
+        stSearchMask.ulDeviceID = ulDeviceID ? 0xFFFFFFFF : 0;
+        stSearchMask.usManufacturerID = usManufacturerID ? 0xFFFF : 0;
+        stSearchMask.usDeviceType = usDeviceType ? 0xFFFF : 0;
+        stSearchMask.ucAuthenticationType = 0;
+        stSearchMask.ucStatusByte1 = 0;//ANTFS_STATUS1_DEFAULT_SEARCH_MASK;
+        stSearchMask.ucStatusByte2 = 0;//ANTFS_STATUS2_DEFAULT_SEARCH_MASK;
+
+        //Search Parameters
+        stSearchParameters.ulDeviceID = ulDeviceID;
+        stSearchParameters.usManufacturerID = usManufacturerID;
+        stSearchParameters.usDeviceType = usDeviceType;
+        stSearchParameters.ucAuthenticationType = 0;
+        stSearchParameters.ucStatusByte1 = 0;
+        stSearchParameters.ucStatusByte2 = 0;
+
+
+        //Step 1: init
+        if ((!hostImp_.Init(0,57600))
+          ||(!WaitForAck(ANTFS_RESPONSE_OPEN_PASS)))
         {
-            return bResult;
+            return FALSE;
         }
+
+        //step 2: Search Device
+        hostImp_.SetChannelID(FR_410_CHANNEL_DEVICE_TYPE,FR_410_CHANNEL_TRANS_TYPE);
+        hostImp_.SetMessagePeriod(FR_410_MESG_PERIOD);
+        hostImp_.SetProximitySearch(5);
+        hostImp_.SetNetworkkey(aucNetworkkey);
+
+        hostImp_.ClearSearchDeviceList();
+        hostImp_.AddSearchDevice(&stSearchMask,&stSearchParameters);
+
+        eReturn = hostImp_.SearchForDevice(FR_410_SEARCH_FREQ,FR_410_LINK_FREQ,0,TRUE);
+
+        if ((!(ANTFS_RETURN_PASS==eReturn))
+          ||(!WaitForAck(ANTFS_RESPONSE_CONNECT_PASS)))
+        {
+            return FALSE;
+        }
+
+        //step 3: Pairing
+
+
+
+        //step 4: Pairing OK:
+        if ((!(ANTFS_RETURN_PASS==eReturn))
+          ||(!WaitForAck(ANTFS_RESPONSE_AUTHENTICATE_PASS)))
+        {
+            return FALSE;
+        }
+        return TRUE;
+
     }
 
     BOOL WaitForAck(ANTFS_RESPONSE expect, int timeout=999)
@@ -22,6 +80,25 @@ public:
         ANTFS_RESPONSE response;
         response = hostImp_.WaitForResponse(timeout);
         return (response == expect);
+    }
+
+    int garmin_ant_read ( garmin_unit * garmin, garmin_packet * p )
+    {
+
+    }
+    int garmin_ant_write ( garmin_unit * garmin, garmin_packet * p )
+    {
+        int r=-1;
+        ANTFS_RETURN eReturn;
+
+        eReturn = hostImp_.ManualTransfer(0xffff,0,1,p->data);
+
+        if((ANTFS_RETURN_PASS == hostImp_.ManualTransfer(0xffff,0,size_t,p->data))
+        && WaitForAck(ANTFS_RESPONSE_MANUAL_TRANSFER_PASS))
+        {
+            r = size_t;
+        }
+        return r;
     }
 };
 
